f82f55977202e1c610b58543ad325ed27c2e3935
JoaquinMontenegro2612 <JoaquinMontenegro2612@gmail.com>
2022-07-22 00:22:40 +0200

    ultimo cambio layout

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-// function saludar()
-// {
-//     let nombre= prompt("Hola,como estas? Me decis tu nombre?");
-//     document.getElementById("saludo").innerHTML=`<p class="text-white text-center ">Hola ${nombre} Bienvenido.</p>`; 
-// }
 let productosPanel = document.getElementById('productos');
 let productos = [{
         "Id": 1,
@@ -61,251 +56,238 @@ let productos = [{
 ];
 
 window.onload = function() {
-        // saludar();
-        if (document.getElementById('productos') !== null) {
-            let productosPanelVista = "";
-            productos.forEach(producto => {
-                {
-                    productosPanelVista +=
-                        ` <div class="container">
-                    <section class="column-xs main-xs-between cross-xs-center full-image-wrap">
-                    <img id="fotoProducto" class="d-block w-100" src="${producto.Foto}">
-                    <h5 id="tituloProducto" class="text-white fs-1">${producto.Nombre}</h5> 
-                    <p id="descripcionProducto" class="text-white fs-1">${producto.Descripcion}</p> 
-                    <p id="precioProducto">$${producto.Precio}</p>
-                    <button class="btn btn-info" onclick="buscarDetalle('${producto.Categoria}','${producto.Id}')">Descripcion</div>
-                    <p id="detalle${producto.Id}"></p>
-                    <div class="input-group p-5 my-1">
-                    <button class="bg-dark text-warning menos input-group-text" onclick="restar('${producto.Id}')">-</button>
-                    <input id="${producto.Id}" type="text" class="bg-dark text-white w-25 text-center" value="0" readonly>
-                    <button class="bg-dark text-warning mas input-group-text" onclick="sumar('${producto.Id}')">+</button>
-                    </div>
-                    <button id="${producto.Id}" onclick="agregar('${producto.Id}')" name="btnComprar" class="btn btn-success">Comprar</button>
-                    </div>   
-                    `
-                }
-
-                document.getElementById('productos').innerHTML = productosPanelVista;
-            });
-            // 
-        };
-        // style="background-image: url=("${producto.Foto}")
-        // ----------------Empieza Filter--------------------------
-
-
-        function filtrar() {
-            let filtro = document.getElementById("filtro").value;
-            let productosPanelVista = "";
-            productos.forEach(producto => {
-                {
-                    if (filtro == "" || producto.Categoria == filtro.toLowerCase()) {
-                        productosPanelVista +=
-                            `<div class="col-12 mb-2 col-md-4 col-sm-4">
-                    <img id="fotoProducto"src="${producto.Foto}" class="img-top h-100 ">
-                    <h5 id="tituloProducto">${producto.Nombre}</h5> 
-                    <p id="descripcionProducto">${producto.Descripcion}</p> 
-                    <p id="precioProducto">$${ producto.Precio }</p>
-                    <button productos-id="${producto.Id}" onclick="agregar('${producto.Id}')" name="btnComprar" class="btn btn-success">Comprar</button>
+    if (document.getElementById('productos') !== null) {
+        let productosPanelVista = "";
+        productos.forEach(producto => {
+            productosPanelVista +=
+                `<section class="container-fluid tmn-bg-section" style="background-image: url(${producto.Foto})">
+                <h5 id="tituloProducto" class="text-white fs-1">${producto.Nombre}</h5> 
+                <p id="descripcionProducto" class="text-white fs-1">${producto.Descripcion}</p> 
+                <p id="precioProducto" class="align-items-end"">$${producto.Precio}</p>
+                <button class="btn btn-info" id="btn-buscar-detalle" onclick="buscarDetalle('${producto.Categoria}','${producto.Id}')">Descripcion</button>
+                <p id="detalle${producto.Id}" class="fs-4"></p>
+                <div class="d-flex input-group justify-content-center align-items-end ">
+                <button class="bg-dark text-warning input-group-text menos" onclick="restar('${producto.Id}')">-</button>
+                <input id="${producto.Id}" type="text" class="bg-dark text-white w-25 text-center input" value="0" readonly></input>
+                <button class="bg-dark text-warning input-group-text mas" onclick="sumar('${producto.Id}')">+</button>
                 </div>
-                </div>
-                </div>
-                `
-                    }
-                }
-                document.getElementById('productos').innerHTML = productosPanelVista;
-            });
-        };
+                <button id="${producto.Id}" onclick="agregar('${producto.Id}')" name="btnComprar" class="btn btn-success">Comprar</button></section>`;
+        });
+        document.getElementById('productos').innerHTML = productosPanelVista;
+    };
+}
 
-        function sumar(bebida) {
-            let valorActual = Number(document.getElementById(bebida).value);
-            valorActual = valorActual + 1;
-            document.getElementById(bebida).value = valorActual;
-        }
+function buscar(texto) {
+    Swal.fire({
+        title: texto,
+        width: 600,
+        padding: '3em',
+        color: '#716add',
+        background: '#fff url(/images/trees.png)',
+        backdrop: `
+        rgba(0,0,123,0.4)
+        url("../imgs/nyan-cat.gif")
+        left top
+        no-repeat
+        `
+    })
+}
+// 
+// ----------------Empieza Filter--------------------------
 
-        function restar(bebida) {
-            let valorActual = Number(document.getElementById(bebida).value);
-            if (valorActual == 0) {
-                return;
+
+function filtrar() {
+    let filtro = document.getElementById("filtro").value;
+    let productosPanelVista = "";
+    productos.forEach(producto => {
+        {
+            if (filtro == "" || producto.Categoria == filtro.toLowerCase()) {
+                productosPanelVista +=
+                    `<div class="col-12 mb-2 col-md-4 col-sm-4">
+        <img id="fotoProducto"src="${producto.Foto}" class="img-top h-100 ">
+        <h5 id="tituloProducto">${producto.Nombre}</h5> 
+        <p id="descripcionProducto">${producto.Descripcion}</p> 
+        <p id="precioProducto">$${ producto.Precio }</p>
+        <button productos-id="${producto.Id}" onclick="agregar('${producto.Id}')" name="btnComprar" class="btn btn-success">Comprar</button>
+    </div>
+    </div>
+    </div>
+    `
             }
-            valorActual = valorActual - 1;
-            document.getElementById(bebida).value = valorActual;
         }
-        // --------------Empieza funcion de agregar en el carrito de compras----------
+        document.getElementById('productos').innerHTML = productosPanelVista;
+    });
+};
 
-        function agregar(idLista) {
-            // agregar un producto a la lista de carrito de compras
-            productos.forEach(producto => {
-                debugger;
-                if (producto.Id == idLista) {
-                    producto.Cantidad = document.getElementById(idLista).value;
-                    actualizarTotal(producto);
-                    if (document.getElementById(producto.Nombre + "_" + idLista) !== null) {
-                        document.getElementById(producto.Nombre + "_" + idLista).remove();
-                    }
-                    document.getElementById('detalleCarrito').innerHTML +=
-                        `
-            <div id="${producto.Nombre}_${idLista}">${producto.Nombre}
+function sumar(bebida) {
+    let valorActual = Number(document.getElementById(bebida).value);
+    valorActual = valorActual + 1;
+    document.getElementById(bebida).value = valorActual;
+}
+
+function restar(bebida) {
+    let valorActual = Number(document.getElementById(bebida).value);
+    if (valorActual == 0) {
+        return;
+    }
+    valorActual = valorActual - 1;
+    document.getElementById(bebida).value = valorActual;
+}
+// --------------Empieza funcion de agregar en el carrito de compras----------
+
+function agregar(idLista) {
+    // agregar un producto a la lista de carrito de compras
+    productos.forEach(producto => {
+        debugger;
+        if (producto.Id == idLista) {
+            producto.Cantidad = document.getElementById(idLista).value;
+            actualizarTotal(producto);
+            if (document.getElementById(producto.Nombre + "_" + idLista) !== null) {
+                document.getElementById(producto.Nombre + "_" + idLista).remove();
+            }
+            document.getElementById('detalleCarrito').innerHTML +=
+                `<div id="${producto.Nombre}_${idLista}">${producto.Nombre}
             <div id="sumaPrecios">Precio $${producto.Precio}<div>
             <div id="cantidad">Cantidad ${producto.Cantidad}<div>
-            <hr color="white" size=5>
-            <div>
-            `
-                }
-            })
-
-        };
-
-        function actualizarTotal(producto) {
-            let subtotal = producto.Precio * producto.Cantidad;
-            document.querySelector("#total").textContent = parseInt(document.querySelector("#total").textContent) + subtotal;
-
-        };
-
-        function buscarDetalle(categoria, id) {
-            const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=";
-            fetch(url + categoria)
-                .then(response => response.json())
-                .then(data => {
-                    let detalle = document.getElementById("detalle" + id);
-                    detalle.innerHTML = data.ingredients[0].strDescription;
-                })
+            <hr color="white" size=5></div>`
         }
-        $(document.body).on('click', 'a[href*="#"]', function(e) {
-            e.preventDefault();
-            $('html,body').animate({
-                scrollTop: $(this.hash).offset().top
-            }, 900);
-        });
+    })
 
-        $(window).on('scroll', function() {
-            var scrolled = $(window).scrollTop();
-            var height = $(window).height();
-            if (scrolled < height) {
-                $('.full-image').css({
-                    'top': -scrolled * .5
-                });
-            }
-        });
-        //-------------------------------------------------------//
-        //-------------------------------------------------------//
-        //-------------------------Empieza Login ----------------//
-        //-------------------------------------------------------//
-        //-------------------------------------------------------//
-        // funciones
-        const btnGuardar = document.getElementById("guardar");
-        const checkbox = document.getElementById("check");
+};
+
+function actualizarTotal(producto) {
+    let subtotal = producto.Precio * producto.Cantidad;
+    document.querySelector("#total").textContent = parseInt(document.querySelector("#total").textContent) + subtotal;
+
+};
+
+function buscarDetalle(categoria, id) {
+    const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=";
+    fetch(url + categoria)
+        .then(response => response.json())
+        .then(data => {
+            // let detalle = document.getElementById("detalle" + id);
+            // detalle.innerHTML = data.ingredients[0].strDescription;
+            buscar(data.ingredients[0].strDescription);
+        })
+}
+
+
+//-------------------------------------------------------//
+//-------------------------------------------------------//
+//-------------------------Empieza Login ----------------//
+//-------------------------------------------------------//
+//-------------------------------------------------------//
+// funciones
+const btnGuardar = document.getElementById("guardar");
+const checkbox = document.getElementById("check");
+const user = document.getElementById("user");
+const pass = document.getElementById("pass");
+let usuarios = [];
+
+if (btnGuardar !== undefined && btnGuardar !== null) {
+    btnGuardar.addEventListener("click", (e) => {
+        e.preventDefault();
+        iniciarSesion(usuariosLS);
+    });
+}
+
+function guardar(valor) {
+    let user = { usuario: email.value, pass: password.value };
+    if (user.usuario == "" || user.pass == "") {
+        password.innerText = "No son tu billetera dejale algo escrito por fa!!";
+        return;
+    } else {
+        if (valor === "sessionStorage") {
+            sessionStorage.setItem("item", JSON.stringify(user));
+        }
+        if (valor === "localStorage") {
+            localStorage.setItem("item", JSON.stringify(user));
+        }
+    }
+}
+
+//function recuperarDatos(){}
+
+function iniciarSesion() {
+    let usuario;
+
+    let usuariosLS = recuperarLS();
+    if (usuariosLS == null) {
+        usuario = crearUsuario();
+        guardarUsuario(usuario);
+        debugger;
+        guardarEnStorage();
+    } else {
         const user = document.getElementById("user");
-        const pass = document.getElementById("pass");
-        let usuarios = [];
-
-        if (btnGuardar !== undefined && btnGuardar !== null) {
-            btnGuardar.addEventListener("click", (e) => {
-                e.preventDefault();
-                iniciarSesion(usuariosLS);
-            });
-        }
-
-        function guardar(valor) {
-            let user = { usuario: email.value, pass: password.value };
-            if (user.usuario == "" || user.pass == "") {
-                password.innerText = "No son tu billetera dejale algo escrito por fa!!";
-                return;
-            } else {
-                if (valor === "sessionStorage") {
-                    sessionStorage.setItem("item", JSON.stringify(user));
-                }
-                if (valor === "localStorage") {
-                    localStorage.setItem("item", JSON.stringify(user));
-                }
-            }
-        }
-
-        //function recuperarDatos(){}
-
-        function iniciarSesion() {
-            debugger;
-            let usuario;
-
-            let usuariosLS = recuperarLS();
-            if (usuariosLS == null) {
-                usuario = crearUsuario();
-                guardarUsuario(usuario);
-                debugger;
-                guardarEnStorage();
-            } else {
-                const user = document.getElementById("user");
-                let usuarioGuardado = usuariosLS.find(userSaved => userSaved.email == user.value);
-                if (usuarioGuardado == null) {
-                    usuario = crearUsuario();
-                    guardarUsuario(usuario);
-                    guardarEnStorage();
-                } else {
-                    const pass = document.getElementById("pass");
-                    if (pass.value == usuarioGuardado.contrasenia) {
-                        window.open("./index.html");
-                    } else {
-                        olvidoPass();
-                    }
-                }
-
-            }
-
-        }
-
-        function crearUsuario() {
-            const user = document.getElementById("user");
+        let usuarioGuardado = usuariosLS.find(userSaved => userSaved.email == user.value);
+        if (usuarioGuardado == null) {
+            usuario = crearUsuario();
+            guardarUsuario(usuario);
+            guardarEnStorage();
+        } else {
             const pass = document.getElementById("pass");
-            let mail = user.value;
-            let password = pass.value;
-            return new Usuario(mail, password);
-        }
-
-        class Usuario {
-            constructor(email, contrasenia) {
-                this.email = email;
-                this.contrasenia = contrasenia;
+            if (pass.value == usuarioGuardado.contrasenia) {
+                window.open("./index.html");
+            } else {
+                olvidoPass();
             }
         }
 
-        function guardarUsuario(usuario) {
-            usuarios.push(usuario);
-        }
+    }
 
-        function guardarEnStorage() {
-            let usuarioJson = JSON.stringify(usuarios);
-            localStorage.setItem("usuarios", usuarioJson);
-        }
+}
 
-        function recuperarLS() {
-            if (localStorage.getItem("usuarios") == null) {
-                return null;
-            }
-            let datos = JSON.parse(localStorage.getItem("usuarios"));
-            return datos;
-        }
+function crearUsuario() {
+    const user = document.getElementById("user");
+    const pass = document.getElementById("pass");
+    let mail = user.value;
+    let password = pass.value;
+    return new Usuario(mail, password);
+}
+
+class Usuario {
+    constructor(email, contrasenia) {
+        this.email = email;
+        this.contrasenia = contrasenia;
+    }
+}
+
+function guardarUsuario(usuario) {
+    usuarios.push(usuario);
+}
+
+function guardarEnStorage() {
+    let usuarioJson = JSON.stringify(usuarios);
+    localStorage.setItem("usuarios", usuarioJson);
+}
+
+function recuperarLS() {
+    if (localStorage.getItem("usuarios") == null) {
+        return null;
+    }
+    let datos = JSON.parse(localStorage.getItem("usuarios"));
+    return datos;
+}
 
 
 
-        function limpiarCampos() {
-            nombre.value = "";
-            userReg.value = "";
-            passReg.value = "";
-            email.value = "";
-        }
+function limpiarCampos() {
+    nombre.value = "";
+    userReg.value = "";
+    passReg.value = "";
+    email.value = "";
+}
 
 
-        // btn olvido de contraseña (este ya anda)
-        function olvidoPass() {
-            Swal.fire({
-                title: 'te olvidaste el password?',
-                text: 'Pues, te jodes',
-                icon: 'error',
-                confirmButtonText: 'Esta funcion no ha sido desarrollada!!'
-            })
-        }
-        // finaliza
-        //------------------Empieza Api-------------//
+// btn olvido de contraseña (este ya anda)
+function olvidoPass() {
+    Swal.fire({
+        title: 'te olvidaste el password?',
+        text: 'Pues, te jodes',
+        icon: 'error',
+        confirmButtonText: 'Esta funcion no ha sido desarrollada!!'
+    })
+}
 
-        fetch('https://www.thecocktaildb.com/api.php')
-            .then((response) => response.json())
-            .then((datos) => console.log(datos));
+// finaliza
